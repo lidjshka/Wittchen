49c809b1da832665216a565c71fc447a1135bf6f
lidjshka <lidjshka@gmail.com>
2018-02-07 15:16:55 +0000

    Initial 2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
-$('.bxslider').bxSlider({
-  auto: true,
-  autoControls: true,
-  stopAutoOnClick: true,
-  pager: true,
-  slideWidth: 600
+$('#submit-message').click(function(ev) {
+    if ($('form')[0].checkValidity()) {
+        ev.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/lidjshka@gmail.com", 
+            method: "POST",
+            data: {
+                Name: $('#name').val(),
+                Tel: $('#tel').val(),
+                Email: $('#email').val(),
+                Message: $('#message').val(),
+            },
+            dataType: "json"
+        }).done(function() {
+            $('form')[0].reset();
+            $('#form-note').html('<p>Спасибо!</p>')
+        }).fail(function(){
+            $('#form-note').html('<p>Ошибка! Заполните поля</p>')
+        });
+    }
 });
